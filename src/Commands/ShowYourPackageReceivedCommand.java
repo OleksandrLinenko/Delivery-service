59cba7ff488570aae0b98d6be6b9ea002b20fa51
oleksandrlinenko <oleksandrlinenko@localhost>
2025-07-24 17:35:30 +0200

    Add ShowYourPackageReceivedCommand class

---
+++ src/Commands/ShowYourPackageReceivedCommand.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Commands;
+
+import DeliveryService.Application;
+import UI.ShowList;
+
+/**
+ *
+ * @author oleksandrlinenko
+ */
+public class ShowYourPackageReceivedCommand {
+        public static ShowYourPackageReceivedCommand create() {
+        return new ShowYourPackageReceivedCommand();
+    }
+    
+    public void handle() {
+        Application.getInstance().getGettingPackages().getListReceived();
+        ShowList.create().handle(Application.getInstance().getGettingPackages());
+    }
+}
