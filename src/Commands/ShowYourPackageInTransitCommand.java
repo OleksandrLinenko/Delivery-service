2041c6492e0b44c4d4d7836a7323d8dd0ef056b7
oleksandrlinenko <oleksandrlinenko@localhost>
2025-07-24 17:35:12 +0200

    Add ShowYourPackageInTransitCommand class

---
+++ src/Commands/ShowYourPackageInTransitCommand.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Commands;
+
+import DeliveryService.Application;
+import UI.ShowList;
+
+/**
+ *
+ * @author oleksandrlinenko
+ */
+public class ShowYourPackageInTransitCommand {
+
+    public static ShowYourPackageInTransitCommand create() {
+        return new ShowYourPackageInTransitCommand();
+    }
+
+    public void handle() {
+        Application.getInstance().getGettingPackages().getListInWay();
+        ShowList.create().handle(Application.getInstance().getGettingPackages());
+    }
+}
