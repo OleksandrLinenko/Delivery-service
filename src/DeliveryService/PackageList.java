d4287c6fadfbcde3300989d68dc24d77c7357664
oleksandrlinenko <oleksandrlinenko@localhost>
2025-07-24 17:33:28 +0200

    Add PackageList class

---
+++ src/DeliveryService/PackageList.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package DeliveryService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author oleksandrlinenko
+ */
+public class PackageList {
+
+    private List<Package> packages = new ArrayList();
+
+    public List<Package> getPackages() {
+        return packages;
+    }
+
+    public void getListInWay() {
+        PackageList result = new PackageList();
+        for (Package packg : packages) {
+            if (inWay(packg)) {
+                addPackage(packg);
+            }
+        }
+    }
+
+    public void getListReceived() {
+        for (Package packg : packages) {
+            if (!inWay(packg)) {
+                addPackage(packg);
+            }
+        }
+    }
+
+    public void addPackage(Package pacgk) {
+        packages.add(pacgk);
+    }
+
+    public boolean inWay(Package packg) {
+        if (packg.getState().equals(State.INWWAY)) {
+            return true;
+        }
+
+        return false;
+    }
+
+}
