9a2f917b36812951e525d0bc9497137455954bd6
oleksandrlinenko <oleksandrlinenko@localhost>
2025-07-27 14:46:24 +0200

    Refactor FindPackageCommand class

--- src/Commands/FindPackageCommand.java
+++ src/Commands/FindPackageCommand.java
@@ -7,7 +7,6 @@ package Commands;
 import DeliveryService.Application;
 import DeliveryService.Package;
 import UI.Ask;
-import UI.ErrorMessage;
 import UI.Message;
 
 /**
@@ -21,15 +20,11 @@ public class FindPackageCommand {
     }
 
     public void handle() {
-        try {
-            Package pack = Application.getInstance().getGettingPackages().findPackage(Ask.create().askInt("Set package ID-number: "));
-            if (pack != null) {
-                Message.create().show(pack.toString());
-            } else {
-                Message.create().show("No package with this ID number");
-            }
-        } catch (Exception ex) {
-            ErrorMessage.create().show(ex.getMessage());
+        Package pack = Application.getInstance().getGettingPackages().findPackage(Ask.create().askInt("Set package ID-number: "));
+        if (pack != null) {
+            Message.create().show(pack.toString());
+        } else {
+            Message.create().show("No package with this ID number");
         }
     }
 }
